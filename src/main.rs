180bc31246b7e2e8db3f9175de10f2c14213f11b
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-15 19:09:38 -0700

    Add replace and split function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use structopt::StructOpt;
 use std::io::{self, Read};
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "rock", about = "A simple command line standard out parser")]
@@ -13,6 +13,30 @@ struct Opt {
     replace: bool,
 }
 
+fn replace(literal: &str, from: &str, to: &str) -> String {
+    return literal.replace(from, to).to_string();
+}
+
+fn split(literal: &str, separator: char) -> Vec<String> {
+    let chars: Vec<char> = literal.chars().collect();
+
+    let mut parts: Vec<String> = Vec::new();
+    let mut index: usize = 0;
+
+    let mut current_part = String::new();
+    while index <= parts.len() {
+        let current_char = chars[index];
+        if current_char == separator {
+            current_part.push(current_char);
+            index += 1;
+        } else {
+            current_part = String::new();
+            parts.push(current_part.to_owned());
+        }
+    }
+    return parts;
+}
+
 fn main() -> io::Result<()> {
     let args = Opt::from_args();
 
@@ -24,4 +48,30 @@ fn main() -> io::Result<()> {
     buffer.pop();
     print!("{}", buffer);
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn replace_test() {
+        assert_eq!(
+            replace("this/will/get/replaced", "/", " "),
+            "this will get replaced"
+        );
+
+        assert_eq!(
+            replace("this will not", "/", " "),
+            "this will not"
+        );
+    }
+
+    #[test]
+    fn split_test() {
+        assert_eq!(
+            split("this/will/get/split", '/'),
+            vec!["this", "will", "get", "replaced"]
+        );
+    }
 }
